d93e302829c894b494715b79b077861f239090a5
Pramod Singh <104872097+Datamathican@users.noreply.github.com>
2025-12-26 23:23:19 +0000

    Create step_trainer_landing.sql

---
+++ sql_queries/step_trainer_landing.sql
@@ -0,0 +1,15 @@
+CREATE EXTERNAL TABLE `step_trainer_landing`(
+  `sensorreadingtime` bigint COMMENT 'from deserializer', 
+  `serialnumber` string COMMENT 'from deserializer', 
+  `distancefromobject` int COMMENT 'from deserializer')
+ROW FORMAT SERDE 
+  'org.openx.data.jsonserde.JsonSerDe' 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION
+  's3://stedi-lakehouse-ps-123/step_trainer_landing'
+TBLPROPERTIES (
+  'classification'='json', 
+  'transient_lastDdlTime'='1766666170')
